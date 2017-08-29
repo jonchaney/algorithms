b7f3d286f4b942e2f6a1271d2f114592ee85fb56
Jonathan Chaney <jonchaney@gmail.com>
2017-08-29 15:31:27 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+    System.out.println(sqrt(49));
+  }
+
+  public static double sqrt (double c) {
+    if (c < 0) return Double.NaN;
+    double err = 1e-15;
+    double t = c;
+    while (Math.abs(t - c/t) > err * t)
+      t = (c/t + t) / 2.0;
+    return t;
+  }
+  
+}
